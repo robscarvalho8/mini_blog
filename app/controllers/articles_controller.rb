c8cbf0ac1b2944969ca84f55b2aa317668b39149
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-11 16:53:02 -0300

    (feature): add method edit to update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,10 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
     @article = Article.find(params[:id])
 
