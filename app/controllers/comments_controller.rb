0d449c33d81a4772f5bdd69aafe6a20e4ca198c8
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-11 17:32:49 -0300

    (refac): delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
     http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-    
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
 
     def destroy
         @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(comment_params)
+        @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article), status: :see_other
     end
